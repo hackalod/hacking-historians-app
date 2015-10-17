2088ceaede8185b006d4901a846bad3e2079e822
Zaturrby <rjkorteschiel@gmail.com>
2015-10-17 13:50:39 +0200

    update full

--- src/entry.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import "babel/polyfill";
-import "./styles/app.scss";
-
-import HackingHistorians from "./components/HackingHistorians.jsx";
-
-ReactDOM.render(<HackingHistorians />, document.body);
